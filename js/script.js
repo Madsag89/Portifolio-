586a4a20d09029377c0d4a54149a6721302a8a2e
Madsag89 <71001617+Madsag89@users.noreply.github.com>
2021-10-21 17:18:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var btn = document.querySelectorAll(".modal-button");
+
+var modals = document.querySelectorAll('.modal');
+
+var spans = document.getElementsByClassName("close");
+
+//Open modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
+}
+
+//Close modal - span
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
+}
+
+//Close modal - click outside of modal container
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
+}
+
+
